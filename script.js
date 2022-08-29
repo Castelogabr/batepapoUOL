2d1577a161e07d579ae43c6cabd6e08f3aa029bf
Gabriel <gabriel.pdg8@gmail.com>
2022-08-29 17:09:13 -0300

    versao final

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let mensagensChat = [];
+let mensagens = [];
 let nick;
+let tipoMensagem = "message";
+let buscarMensagens = true;
 
 Usuario()
 function Usuario(){
@@ -21,50 +23,101 @@ function falhaUsuario(error){
 function usuarioOK(){
     entrarNaSala();
 }
-function entrarNaSala(){
-    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    promessa.then(mostrarMensagem);
-    promessa.catch();
+
+
+function entrarNaSala() {
+  carregarMensagens();
+  
+  AtualizacaoDeMensagens();
+  AtualizacaoDeStatus();
 }
-function mostrarMensagem(resposta){
 
-    mensagensChat = resposta.data
-    console.log(mensagensChat)
+function carregarMensagens() {
+  if (!buscarMensagens) return;
 
-     const ul = document.querySelector('.mensagens');
-      
-    ul.innerHTML = '';
-
-    for(let i = 0; i < mensagensChat.length; i++){
-
-        ul.innerHTML = ul.innerHTML + `
-            <li>
-            (${mensagensChat[i].time}) <strong> ${mensagensChat[i].from}</strong>  para  <strong> ${mensagensChat[i].to}</strong>: ${mensagensChat[i].text}
-            </li>
-        `;
-    }
+  const promisse = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+  promisse.then(processarMensagens);
+}
 
 
-} 
+function AtualizacaoDeMensagens() {
+  setInterval(carregarMensagens, 3000);
+}
 
-function processarParticipantes(resposta) {
-    participantes = resposta.data;
-  
-    const todos = { name: "Todos" };
-    participantes.unshift(todos);
-  
-    renderizarParticipantes();
+function AtualizacaoDeStatus() {
+  setInterval(atualizarStatus, 5000);
+}
+
+function atualizarStatus() {
+  const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/status');
+}
+
+function processarMensagens(resposta) {
+  mensagens = [];
+
+  for (let i = 0; i < resposta.data.length; i++) {
+    const mensagem = resposta.data[i];
+    mensagens.push(mensagem);
   }
 
-function carregarParticipantes() {
-    const requisicao = axios.get(path+"/participants", {
-      headers: {
-        User: nome
-      }
-    });
-    requisicao.then(processarParticipantes);
+  renderizarMensagens();
+}
+
+let message
+
+
+const classesMensagens = {
+    status: 'entrada-saida',
+    message: 'conversa-publica'
+};
+
+function renderizarMensagens() {
+  const ul = document.querySelector(".mensagens-container");
+  let html = "";
+
+  for (let i = 0; i < mensagens.length; i++) {
+    const mensagem = mensagens[i];
+    if(mensagens[i].type === "message"){ 
+      ul.innerHTML = ul.innerHTML +`
+      <li class="'conversa-publica ${classesMensagens[mensagem.type]}">
+        <div class="conteudo-mensagem">
+        <span class="horario">(${mensagem.time})</span>
+          <span><strong>${mensagem.from}</strong></span>
+            <span> para </span>
+          <strong>${mensagem.to}</strong>:
+          <span class="text">${mensagem.text}</span>
+        </div>
+      </li>
+    `;
+  }
+  if(mensagens[i].type === "status"){ 
+    ul.innerHTML = ul.innerHTML +`
+    <li class="entrada-saida ${classesMensagens[mensagem.type]}">
+      <div class="conteudo-mensagem">
+      <span class="horario">(${mensagem.time})</span>
+        <span><strong>${mensagem.from}</strong></span>
+        <span class="text">${mensagem.text}</span>
+      </div>
+    </li>
+  `;
+}
+
+  }
+  setTimeout(() => document.querySelector(".mensagens-container li:last-child").scrollIntoView(), 0);
+}
+
+function enviarMensagem(){
+  const msg = document.querySelector(".input-mensagem").value;
+  const objetoMsg = {
+      from: nome,
+      to: "Todos",
+      text: msg,
+      type: "message"
   }
 
-function setAttParticipantes() {
-    setInterval(carregarParticipantes, 10000);
-  }
+  const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", objetoMsg);
+
+  promessa.then(()=>{carregarMensagens()});
+  promessa.catch(()=>{window.location.reload()});
+
+}
